5107404596ccc05e9ccb350895a5715397cd274d
Nicolas Rabecq <nicolasrabecq@gmail.com>
2015-09-05 11:43:15 +0200

    no message

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 var express = require('express'),
-lessMiddleware = require('less-middleware'),
+less = require('less'),
+fs = require('fs'),
 path = require('path'),
+i18n = require('i18next'),
 app = express();
+
+var cssPath = 'public/css/';
 
 app.set('view engine', 'ejs');
 
@@ -13,8 +17,16 @@ app.get('/about', function(req, res){
 	res.render('pages/about');
 });
 
+(function() {
+    var src = cssPath + 'styles.less';
+    less.render(fs.readFileSync(src).toString(), {
+        filename: path.resolve(src),
+    }, function(e, output) {
+		fs.writeFileSync(cssPath + 'styles.css', output.css, 'utf8');
+    });
+})();
 
-
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/client', express.static(__dirname + '/../client'));
 
 app.listen(8080);
