8ec5f2448ea0e6d50f2bbb411867e5ff747e3158
Nicolas Rabecq <nicolasrabecq@gmail.com>
2015-09-05 14:01:16 +0200

    no message

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -1,57 +1,12 @@
 angular.module("appMovie").controller("mainController", function($scope, $http, $location, $log, $interval, moviesFactory) {
 	
-	$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+	$http.get('/').then(function(res){
+		$scope.movies = res.data;
+	});
 	
-	$scope.count = [];
-	$scope.list = [];
-	
-	$scope.getCounts = function() {
-		
-		moviesFactory.getCountMovies().success(function(data){
-			$scope.count.movies = data;
-		});
-		
-		moviesFactory.getCountAuthors().success(function(data){
-			$scope.count.authors = data;
-		});
-		
-		moviesFactory.getCountGenders().success(function(data){
-			$scope.count.genders = data;
-		});
-		
-		moviesFactory.getCountActors().success(function(data){
-			$scope.count.actors = data;
-		});
-	
-	}
-	
-	$scope.getLists = function() {
-		
-		moviesFactory.getAuthorList().success(function(authors){
-			$scope.list.author = authors;
-		});
-		
-		moviesFactory.getActorList().success(function(actors){
-			$scope.list.actor = actors;
-		});
-		
-		moviesFactory.getGenderList().success(function(genders){
-			$scope.list.gender = genders;
-		});
-		
-	}
-	
-	$scope.movieList = function() {
-		
-		moviesFactory.getMovieList().success(function(movies){
-			$scope.movieList = movies;
-		});
-		
-	}
-	
-	$scope.intervalPromise = $interval(function(){
+	/*$scope.intervalPromise = $interval(function(){
 		$scope.getCounts();
-	}, 60000)	
+	}, 60000)*/	
 	
 	function resultRequestTrue() {
 		var $content = '<div class="request" style="width:60px;height:60px;box-sizing:border-box;z-index:2;background:#6DEA93;margin:auto;border-radius:5px;"><i class="fa fa-check" style="font-size:50px;padding:5px;"></i><div>';
@@ -72,42 +27,5 @@ angular.module("appMovie").controller("mainController", function($scope, $http, 
 			$('.request').remove();
 		}, 5000);
 	}
-	
-	$scope.controlAddMovie = function(movie) {
-		
-		var $title = movie.title;
-		var $author = movie.author;
-		var $actor = movie.actor
-		var $year = movie.year;
-		var $gender = movie.gender;
-		var $comment = movie.comment;
-		
-		$http.post('webservice/setMovie.php',
-		{
-			'title': $title,
-			'author': $author,
-			'actor': $actor,
-			'year': $year,
-			'gender': $gender,
-			'comment': $comment
-		}).then(function(response){
-			console.log('Success');
-			resultRequestTrue();
-		}, function(response){
-			console.log('Error');
-			resultRequestFalse();
-		});
-		
-	}	
-	
-	console.log($location);
-	
-	if($location.absUrl().indexOf('adMovie') > -1) {
-		$scope.getLists();
-	} else {
-		$scope.movieList();
-	}
-	
-	$scope.getCounts();
 	
 });
--- server/server.js
+++ server/server.js
@@ -1,22 +1,16 @@
 var express = require('express'),
 less = require('less'),
 fs = require('fs'),
+mysql = require('mysql'),
 path = require('path'),
 i18n = require('i18next'),
 app = express();
 
 var cssPath = 'public/css/';
+var movies = [];
 
-app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-	res.render('pages/index');
-});
-
-app.get('/about', function(req, res){
-	res.render('pages/about');
-});
-
+/** Configure LESS CSS */
 (function() {
     var src = cssPath + 'styles.less';
     less.render(fs.readFileSync(src).toString(), {
@@ -26,7 +20,39 @@ app.get('/about', function(req, res){
     });
 })();
 
+/** Configure mysql connection */
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    port     : '8889',
+    password : 'root',
+    database : 'movie'
+});
+connection.connect();
+
+
+/** app set */
+app.set('view engine', 'ejs');
+
+/** app get */
+app.get('/', function(req, res){
+    res.render('pages/index');
+    connection.query('SELECT * FROM movie', function(err, rows, fields) { 
+        //res.end(JSON.stringify(rows));
+        res.json(rows);
+    });
+});
+app.get('/movie', function(req, res){
+	res.render('pages/movie');
+});
+app.get('/about', function(req, res){
+	res.render('pages/about');
+});
+
+/** app use */
 app.use('/static', express.static(__dirname + '/public'));
 app.use('/client', express.static(__dirname + '/../client'));
+
+
 
 app.listen(8080);
