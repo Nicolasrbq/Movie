dc85d622472f88d6e983e444b88fdef3107b5e0c
Nicolas Rabecq <nicolasrabecq@gmail.com>
2015-09-12 14:53:58 +0200

    no message

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
 var express = require('express'),
-less = require('less'),
+less = require('./my_modules/less'),
 fs = require('fs'),
 mysql = require('mysql'),
 path = require('path'),
 i18n = require('i18next'),
+
 app = express();
-
-var cssPath = 'public/css/';
-
 
 app.configure( function(){
     app.engine('html', require('ejs').renderFile);
 });
 
-/** Configure LESS CSS */
-(function() {
-    var src = cssPath + 'styles.less';
-    less.render(fs.readFileSync(src).toString(), {
-        filename: path.resolve(src),
-    }, function(e, output) {
-		fs.writeFileSync(cssPath + 'styles.css', output.css, 'utf8');
-    });
-})();
+less.getLess();
 
 /** Configure mysql connection */
 var connection = mysql.createConnection({
